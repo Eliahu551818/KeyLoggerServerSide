750a4386d25d0c478c0dd2f39ef7790720928e3a
eliahu <eliahubenamotz@gmail.com>
2025-02-21 01:44:59 +0200

    gunicorn server and last update for server

--- fastapi_server/main.py
+++ fastapi_server/main.py
@@ -18,4 +18,4 @@ app.include_router(data_router)
 app.include_router(users_router)
 
 if __name__ == '__main__':
-    uvicorn.run(app=app)
+    uvicorn.run(app=app, host='0.0.0.0', port=8000, reload=True )
