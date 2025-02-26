8626a55b57fbc06900b992ab350e5ac651a9b1e9
shale <s0534156555@gmail.com>
2025-02-26 15:13:22 +0200

    homepage

--- flask_server/app.py
+++ flask_server/app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/", methods=['GET'])
 def homePage():
-    return render_template("HomePage.html")
+    return render_template("home-page.html")
 
 @app.route('/dashboard')
 def dashboard():
